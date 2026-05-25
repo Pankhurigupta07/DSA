4b0eb3d73452ed55c33113a1bf81d2a134137c47
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-05-25 15:21:22 +0530

    pattern2,3

--- pattern.1.py
+++ pattern.1.py
@@ -1,6 +1,34 @@
-for i in range(1,5):
-    for j in range(1,5):
-        print("*", end=" ")
 
-    print("\n")
+# def pattern1(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print("*", end="")
+
+#         print("\n")
+
+# n=int(input())
+# pattern1(n)
     
+
+
+# def pattern2(n):
+#     for i in range(n+1):
+#         for j in range(i):
+#             print("*", end=" ")
+
+#         print()
+
+# n=int(input())
+# pattern2(n)
+    
+
+def pattern3(n):
+    for i in range(n+1):
+        for j in range(i):
+            print(j+1,end="")
+
+        print()
+
+n=int(input())
+pattern3(n)
+   
