3d8e3ed676f3eaf42c7618386420cac91cb197fd
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-05-26 15:24:36 +0530

    pattern 1,2,3,4,5,6,7

--- pattern.1.py
+++ pattern.1.py
@@ -22,13 +22,63 @@
 # pattern2(n)
     
 
-def pattern3(n):
-    for i in range(n+1):
-        for j in range(i):
-            print(j+1,end="")
+# def pattern3(n):
+#     for i in range(n+1):
+#         for j in range(i):
+#             print(j+1,end="")
 
-        print()
+#         print()
 
-n=int(input())
-pattern3(n)
+# n=int(input())
+# pattern3(n)
    
+
+
+# def pattern4(n):
+#     for i in range(n+1):
+#         for j in range(i):
+#             print(i,end="")
+
+#         print()
+
+# n=int(input())
+# pattern4(n)
+   
+
+# def pattern5(n):
+#     for i in range(n):
+#         for j in range(n-i):
+#             print("*",end="")
+
+#         print()
+
+# n=int(input())
+# pattern5(n)
+  
+
+
+# def pattern6(n):
+#     for i in range(n):
+#         for j in range(n-i):
+#             print(j+1,end="")
+
+#         print()
+
+# n=int(input())
+# pattern6(n)
+  
+
+# def pattern7(n):
+#     for i in range(n):
+#         for j in range(n-i-1):
+#             print(" ",end="")
+#         for k in range(2*i+1):
+#             print("*", end="")
+#         for l in range(n-i-1):
+#             print(" ",end="")
+
+#         print()
+
+# n=int(input())
+# pattern7(n)
+
