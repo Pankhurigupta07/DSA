4c80a53005d2e0d93c6268389191c8e54b5c7ae5
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-05-29 15:21:10 +0530

    Basic maths concept

---
+++ Basic_maths_concepts.py
@@ -0,0 +1,34 @@
+# count the digits 
+# def count_digit(n):
+#     count=0
+#     while(n>0):
+#         # lastdigit=n%10
+#         count=count+1
+#         n=n//10
+#     return count
+# n=int(input())
+# print(count_digit(n))
+
+
+# 2nd type to solve the same question
+
+# def count_digit(n):
+#     count=int(log10(n)+1)
+#     return count
+
+# n=int(input())
+# print(count_digit(n))
+
+# reverse the numbers
+def reverse_input_numbers(n):
+    
+    while(n>0):
+        lastdigit=n%10
+    
+        n=n//10
+    return l
+
+n=int(input())
+print(reverse_input_numbers(n))
+
+
