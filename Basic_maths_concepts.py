624edff36fe5bc851df701c29f87303323159f20
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-05-29 19:16:55 +0530

    Basic maths conceptt

--- Basic_maths_concepts.py
+++ Basic_maths_concepts.py
@@ -10,7 +10,7 @@
 # print(count_digit(n))
 
 
-# 2nd type to solve the same question
+#2nd type to solve the same question
 
 # def count_digit(n):
 #     count=int(log10(n)+1)
@@ -20,15 +20,45 @@
 # print(count_digit(n))
 
 # reverse the numbers
-def reverse_input_numbers(n):
+# def reverse_input_numbers(n):
     
-    while(n>0):
-        lastdigit=n%10
+#     while(n>0):
+#         lastdigit=n%10
+#         print(lastdigit,end="")
+#         n=n//10
     
-        n=n//10
-    return l
+# n=int(input())
+# reverse_input_numbers(n)
 
-n=int(input())
-print(reverse_input_numbers(n))
+#another way to solve same problem
+
+# def reverse_input_numbers(n):
+#     reverse=0
+#     while(n>0):
+#         lastdigit=n%10
+#         n=n//10
+#         reverse=(reverse*10)+lastdigit
+#     return reverse
+
+# n=int(input())
+# print(reverse_input_numbers(n))
 
 
+
+# def reverse_input_numbers(n):
+#     reverse=0
+#     while(n>0):
+#         lastdigit=n%10
+#         n=n//10
+#         reverse=(reverse*10)+lastdigit
+#     return reverse
+
+# n=int(input())
+# result=(reverse_input_numbers(n))
+# if result==n:
+#     print(True)
+# else:
+#     print(False)
+
+    
+  
