69740f7ca7f8fb54f4a1e11891a2314167b7c68c
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-05-31 13:37:25 +0530

    Recursionn

--- Recurssion.py
+++ Recurssion.py
@@ -1,12 +1,37 @@
 # print name n times
-def print_name(name,i):
-    if(i==5):
-        return
-    else:
-        print(name,end=" ")
-        i=i+1
-        print_name(name,i)
+# def print_name(name,i):
+#     if(i==5):
+#         return
+#     else:
+#         print(name,end=" ")
+#         i=i+1
+#         print_name(name,i)
 
-n=input()
-i=0
-print_name(n,i)
+# n=input()
+# i=0
+# print_name(n,i)              # time complexity and sc=O(n)
+
+
+
+# print 1 to n
+# def print_number(i,n):
+#     if(i>n):
+#         return
+#     print(i)
+#     i=i+1
+#     print_number(i,n)
+
+# n=int(input())
+# print_number(1,n)
+
+
+# print from n to 1
+# def print_number(n,i):
+#     if(n<i):
+#         return
+#     print(n)
+#     n=n-1
+#     print_number(n,i)
+
+# n=int(input())
+# print_number(n,1)
