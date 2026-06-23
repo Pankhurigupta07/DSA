8c1ec1b0b34a925644096f662dec3459b54ba6aa
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-06-23 18:02:02 +0530

    Sorting

---
+++ Sorting_1.py
@@ -0,0 +1,13 @@
+# insertion sort 
+
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        mini=i
+        for j in range(i,n):
+            if arr[j]<arr[mini]:
+                arr[mini],arr[j]=arr[j],arr[mini]
+    return arr
+
+arr=[int(x) for x in input().split()]
+print(selection_sort(arr))
