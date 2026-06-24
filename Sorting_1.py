e24fd727468c56b4a27cc8be078b9063ffe289f9
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-06-24 13:20:36 +0530

    Sorting

--- Sorting_1.py
+++ Sorting_1.py
@@ -37,3 +37,17 @@
 
 # worst time complexity= O(n^2)
 # best time complexity=O(n)
+
+
+#insertion sort
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        j=i
+        while(j>0 and arr[j-1]>arr[j]):
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+    return arr
+        
+
+arr=[int(x)for x in input().split()]
+print(insertion_sort(arr))
