a74fe92022c0360a3ff6c67f1de8458af2ccda8a
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-06-24 20:09:04 +0530

    Sorting_2

---
+++ Sorting_2.py
@@ -0,0 +1,36 @@
+# merge sort
+def merge(arr,low,mid,high):
+    left=low
+    right=mid+1
+    temp=[]
+    while(left<=mid and right<=high):
+        if(arr[left]<=arr[right]):
+            temp.append(arr[left])
+            left+=1
+        else:
+            temp.append(arr[right])
+            right+=1
+
+    while(left<=mid):
+        temp.append(arr[left])
+        left+=1
+
+    while(right<=high):
+        temp.append(arr[right])
+        right+=1
+    
+    for i in range(len(temp)):
+        arr[low+i]=temp[i]
+
+def merge_sort(arr,low,high):
+    if (low>=high):
+        return
+    mid=(low+high)//2
+    merge_sort(arr,low,mid)
+    merge_sort(arr,mid+1,high)
+    merge(arr,low,mid,high)
+    return arr
+
+arr=[int(x) for x in input().split()]
+n=len(arr)
+print("Sorted Array:", merge_sort(arr,0,n-1))
