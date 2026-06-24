793702e76a01e8bfed23b98f703deecf4031b135
Pankhurigupta07 <pankhurigupta768@gmail.com>
2026-06-24 21:55:18 +0530

    Sorting_2

--- Sorting_2.py
+++ Sorting_2.py
@@ -34,3 +34,32 @@
 # arr=[int(x) for x in input().split()]
 # n=len(arr)
 # print("Sorted Array:", merge_sort(arr,0,n-1))
+
+#best average and worst tc=O(nlogn)
+#space complexity=O(n)
+
+
+# recursive bubble sort
+def bubble_sort_rec(arr, n):
+    
+    if n == 1:
+        return
+
+    swapped = False
+    
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            swapped = True  
+
+    
+    if not swapped:
+        return
+
+   
+    bubble_sort_rec(arr, n - 1)
+
+
+arr = [int(x) for x in input().split()]
+bubble_sort_rec(arr, len(arr))
+print("Sorted Array:", arr)
